8eb99a4381912efeb21989a90cf7b60042cab904
Paulo R S Malheiros <paulorsmalheiros@gmail.com>
2024-02-27 17:17:20 -0300

    iniciando curso de dev android ifce

---
+++ Conceitos_iniciais/src/main/kotlin/capacidade-var.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("ULong maximo:${ULong.MAX_VALUE} / ULong mÍnimo:${ULong.MIN_VALUE}")
+    println("UInt maximo:${UInt.MAX_VALUE} / UInt mÍnimo:${UInt.MIN_VALUE}")
+    println("UShort maximo:${UShort.MAX_VALUE} / UShort mÍnimo:${UShort.MIN_VALUE}")
+    println("Long maximo:${Long.MAX_VALUE} / long mÍnimo:${Long.MIN_VALUE}")
+
+}
---
+++ Conceitos_iniciais/src/main/kotlin/var-2-val.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var t:Int = 19
+//  val = quando nao vai declarar
+//  var = quando vai declarar
+
+    var str = "curso odex"
+    var j=0
+        while(j<str.length){
+            print("${j} ")
+            j++
+        }
+    print("\n ${str[5]}")
+}
